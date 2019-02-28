7763ee2558b739e3d55995245d6b92f60e0e85ed
AndreaB2604 <andreabugin@hotmail.it>
2019-02-28 19:40:16 +0100

    Create the first version of main.c and tsp.c

---
+++ main.c
@@ -0,0 +1,56 @@
+#include "tsp.h"
+
+/*   PROTOTYPES   */
+void parse_command_line(int argc, char** argv, instance *inst);
+void read_input(instance *inst);
+
+/*   MAIN   */
+int main(int argc, char **argv)
+{
+	instance inst;
+	if(argc < 2)
+	{
+		printf("Usage: %s -help for help\n", argv[0]);
+		exit(1);
+	}
+
+	parse_command_line(argc, argv, &inst);
+
+	read_input(&inst);
+	return 0;
+}
+
+
+void parse_command_line(int argc, char** argv, instance *inst) 
+{ 
+	int i;
+	if(VERBOSE >= 100)
+		printf(" running %s with %d parameters\n", argv[0], argc-1); 
+		
+	// default values
+	strcpy(inst->input_file, "NULL");
+	inst->time_limit = 2147483647;	// random number for now
+
+	int help = 0;
+	if(argc < 1) { help = 1; }
+	for(i=1; i<argc; i++)
+	{
+		if(!strcmp(argv[i], "-file")) { strcpy(inst->input_file,argv[++i]); }				// input file
+		else if(!strcmp(argv[i],"-f")) { strcpy(inst->input_file,argv[++i]); }				// input file
+		else if(!strcmp(argv[i],"-time_limit")) { inst->time_limit = atof(argv[++i]); }		// total time limit
+		else if(!strcmp(argv[i], "-help") || !strcmp(argv[i], "--help")){ help = 1; }		// help
+		else help = 1;
+	}
+
+	if(help || (VERBOSE >= 10))
+	{
+		printf("\n\n");
+		printf("available parameters ---------------------------------------------------\n");
+		printf("-file %s\n", inst->input_file); 
+		printf("-time_limit %lf\n", inst->time_limit); 
+		printf("\nenter -help or --help for help\n");
+		printf("------------------------------------------------------------------------\n\n");
+	}
+
+	if(help) exit(1);
+}
---
+++ tsp.h
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define VERBOSE 50 // printing level  (=10 only incumbent, =20 little output, =50-60 good, =70 verbose, >=100 cplex log) 
+#define FILE_SIZE 1000 // maximum file size we accept
+
+typedef struct
+{
+	// input data
+	int nnodes;
+	double *xcoord; 								// they could be integer
+	double *ycoord;
+
+	// parameters
+	double time_limit; 								// long can be used
+	char input_file[FILE_SIZE];
+	double *best_sol; 								// best solution available
+} instance;
+
